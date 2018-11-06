d2ec640d018a56b936de4bc43ae6a19d61c6bc22
Viacheslav Ostrovskiy <chelout@gmail.com>
2018-11-06 10:17:47 +0300

    SubscriptionConfirmation

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,46 @@ Route::get('/', function () {
 });
 
 Route::post('sns', function (Request $request) {
-    return $request->all();
+    $messageType = $request->header('x-amz-sns-message-type');
+
+    switch ($messageType) {
+        case 'SubscriptionConfirmation':
+            $client = new Client([
+                // You can set any number of default request options.
+                'timeout'  => 2.0,
+            ]);
+
+            $type = $request->get('Type');
+            $messageId = $request->get('MessageId');
+            $token = $request->get('Token');
+            $topicArn = $request->get('TopicArn');
+            $message = $request->get('Message');
+            $subscribeURL = $request->get('SubscribeURL');
+            $timestamp = $request->get('Timestamp');
+            $signatureVersion = $request->get('SignatureVersion');
+            $signature = $request->get('Signature');
+            $signingCertURL = $request->get('SigningCertURL');
+
+            $response = $client->get($subscribeURL);
+            
+            Log::info($response);
+            
+            // dd($response);
+
+            break;
+
+        case 'Notification':
+            # code...
+            break;
+
+        case 'UnsubscribeConfirmation':
+            # code...
+            break;
+            
+        default:
+            # code...
+            break;
+    }
+
+    // return $request->all();
 })->middleware(\Chelout\HttpLogger\Middlewares\HttpLogger::class);
